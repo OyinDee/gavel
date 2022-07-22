d0eacd7cab694f2dd1b5c8f73c93e8929f675a7e
excellence-arch <oladipupomichael9@gmail.com>
2022-07-22 20:34:09 +0100

    added the login and register buttons to the navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./layouts/Navbar";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
