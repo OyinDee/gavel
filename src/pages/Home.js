dfce0f94035478d119fa726894bac4d47a1cc3a1
Timimah <timimah0117@gmail.com>
2022-07-23 05:34:26 +0100

    working on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home_background text-center'>
+      <div className='fs-5 text text-light'>Justice for One, Justice for all</div>
+      <div className='fs-1 text text-light mb-4'>LET US BRIDGE THE GAP FOR YOU</div>
+      <button className='btn text-light btn-outline-gavel fs-3'>Register now</button> 
+    </div>
   )
 }
 
