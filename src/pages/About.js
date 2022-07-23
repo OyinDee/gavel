e75b8838a21d8ecff330731f20d8789b04448560
Timimah <timimah0117@gmail.com>
2022-07-23 03:13:02 +0100

    adjusted the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,14 +6,15 @@ const About = () => {
     <div className="a_container">
         <div className="w-50 card text-center" id='about_container'>
             <h1 className='card-title'>About Us</h1>
+            <hr/>
             <div style={{padding: '10px', textAlign: 'center'}}>
-                <h2>OUR MISSION</h2>
+                <h4>OUR MISSION</h4>
                 To promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels.
                 <br/><br/>
-                <h2>OUR VISION</h2>
+                <h4>OUR VISION</h4>
                 To ensure public access to information and protect fundamental freedoms, prevent violence and combat terrorism and crime and ensure equal access to justice for all.
                 <br/><br/>
-                <h2>OUR GOAL</h2>
+                <h4>OUR GOAL</h4>
                 To connect potential clients with lawyers without going through stress. We aim to provide a platform where people can air their grievances and get heard.
             </div>
         </div>
