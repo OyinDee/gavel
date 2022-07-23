aaab6510664cb528315c851d5f4f6bc84471f64e
excellence-arch <oladipupomichael9@gmail.com>
2022-07-23 08:54:10 +0100

    design in process

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom';
 import Navbar from '../layouts/Navbar';
 import './Home.css'
 
@@ -12,7 +13,7 @@ const Home = () => {
       <div className='home_background text-center'>
         <div className='fs-5 text text-light'>Justice for One, Justice for all</div>
         <div className='fs-1 text text-light'>LET US BRIDGE THE GAP FOR YOU</div>
-        <button className='btn text-light btn-outline-gavel fs-3 px-4'>Register now</button> <br />
+        <Link className='btn text-light btn-outline-gavel fs-3 px-4' to="/register">Register now</Link> <br />
         <button className='btn btn-warning text-light fs-3 px-5 mt-2'>Distress call</button> 
 
       </div>
